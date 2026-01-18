57b5c43dd63eba1f96ccc5760f053bbdba653f1f
Tushar Ravaliya <tushar.ravaliya18@gmail.com>
2026-01-18 17:44:12 +0530

    update contact information in About, Contact, Footer, and Hero components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   return (
     <div id="contact" className="border-b border-neutral-900 pb-20">
       <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
       <div className="text-center tracking-tighter">
-        <p className="my-4 text-neutral-400">123 Street, City, Country</p>
-        <p className="my-4 text-neutral-400">+1 234 567 890</p>
-        <a href="mailto:email@example.com" className="border-b border-neutral-500 my-4 text-neutral-400 hover:text-white transition-colors">email@example.com</a>
+        <p className="my-4 text-neutral-400">Rajkot, India</p>
+        <p className="my-4 text-neutral-400">+91 234 567 890</p>
+        <a
+          href="mailto:tushar.ravaliya18@gmail.com"
+          className="border-b border-neutral-500 my-4 text-neutral-400 hover:text-white transition-colors"
+        >
+          tushar.ravaliya18@gmail.com
+        </a>
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className="text-center p-4 text-neutral-500 text-sm">
-            <p>&copy; {new Date().getFullYear()} Tushal. All rights reserved.</p>
-        </div>
-    );
+  return (
+    <div className="text-center p-4 text-neutral-500 text-sm">
+      <p>
+        &copy; {new Date().getFullYear()} Tushar Ravaliya. All rights reserved.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
